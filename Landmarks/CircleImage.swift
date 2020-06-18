604983ff8819737431e74624ea4d332c0918aeea
Kiren Srinivasan <srinitude@gmail.com>
2020-06-18 11:16:49 -0400

    Adds initial ContentView for a Landmark

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Kiren Srinivasan on 6/18/20.
+//  Copyright © 2020 Practice. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4)
+            )
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
