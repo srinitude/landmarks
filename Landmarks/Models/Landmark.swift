fe2998bc3ccfc7f51cba8c709fee467375a96960
Kiren Srinivasan <srinitude@gmail.com>
2020-06-18 17:57:02 -0400

    Adds more changes

--- Landmarks/Models/Landmark.swift
+++ Landmarks/Models/Landmark.swift
@@ -17,6 +17,7 @@ struct Landmark: Hashable, Codable, Identifiable {
     var state: String
     var park: String
     var category: Landmark.Category
+    var isFavorite: Bool
     
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
